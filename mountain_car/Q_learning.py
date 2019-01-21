c7841227cdb26467650ee8610406f370dd0e69bb
Shunichi09 <quick1st97@gmail.com>
2019-01-21 17:58:40 +0900

    complete Q_learn/mountain

--- mountain_car/Q_learning.py
+++ mountain_car/Q_learning.py
@@ -1,19 +1,64 @@
+from collections import defaultdict
+import numpy as np
+import matplotlib.pyplot as plt
 import gym
-env = gym.make('MountainCar-v0')
+from gym import wrappers
+
+"""
+About Q
+Q = dict
+{state : [ ,  ,  ,  ] } 
+"""
+
+def discretize_state(state, env):
+    """discretize agent state
+    Parameters
+    ------------
+    state : list
+    env : gym class
+    Returns
+    --------
+    position : int
+    velocity : int
+    """
+    state_num = 40
+    env_low = env.observation_space.low # 位置と速度の最小値
+    env_high = env.observation_space.high #　位置と速度の最大値
+    env_dx = (env_high - env_low) / state_num # 40等分
+    # 0〜39の離散値に変換する
+    position = int((state[0] - env_low[0])/env_dx[0])
+    velocity = int((state[1] - env_low[1])/env_dx[1])
+
+    return [position, velocity]
 
 class BaseAgent():
-
+    """Base Agent
+    Attributes
+    -----------
+    Q : dict
+    epsilon : float
+    reward_log : list
+    """
     def __init__(self, epsilon):
         self.Q = {}
         self.epsilon = epsilon
         self.reward_log = []
 
-    def policy(self, s, actions):
+    def policy(self, state, actions):
+        """epsilon greedy 
+        Parameters
+        -----------
+        state : list
+            state of list 
+        actions : list
+            action list
+        """
+        # print(self.Q[state[0], state[1]])
         if np.random.random() < self.epsilon:
             return np.random.randint(len(actions))
         else:
-            if s in self.Q and sum(self.Q[s]) != 0:
-                return np.argmax(self.Q[s])
+            if tuple(state) in self.Q and sum(self.Q[state[0], state[1]]) != 0:
+                return np.argmax(self.Q[state[0], state[1]])
             else:
                 return np.random.randint(len(actions))
 
@@ -54,26 +99,67 @@ class QlearningAgent(BaseAgent):
     """
 
     """
-    def __init__(self):
+    def __init__(self, epsilon=0.1):
         """
-
-        
-        """
-        epsilon = 0.1
+        """ 
         super().__init__(epsilon)
     
-    def learn(self, env, episode_count=1000, gamma=0.9, learning_rate=0.1, render=False, report_interval=50):
+    def learn(self, env, episode_count=10000, gamma=0.99, learning_rate=0.2, render=False, report_interval=100):
         """
         Parameters
         -----------
         env : 
 
 
-
         """
+        # step1 initialize
+        self.init_log()
+        actions = list(range(env.action_space.n))
+        self.Q = defaultdict(lambda : [0] * len(actions))
 
+        # step2 learning
+        for e in range(episode_count): # 何エピソード行うか
+            print("episode = {}".format(e))
+            state = env.reset() # 状態初期化
+            state = discretize_state(state, env)
+            game_end_flg = False
+            
+            while not game_end_flg:
+                if render: # 描画かどうかの確認
+                    env.render()
+                
+                action = self.policy(state, actions) # epsilon greedy
+                next_state, reward, game_end_flg, info = env.step(action) # open AI gym の返り値（観測結果・報酬・ゲーム終了FLG・詳細情報を取得） 
+                next_state = discretize_state(next_state, env)
 
+                # update
+                gain = reward + gamma * max(self.Q[next_state[0], next_state[1]]) # 次の状態での最大値・これプログラム綺麗
+                estimated = self.Q[state[0], state[1]][action] 
+                self.Q[state[0], state[1]][action] += learning_rate * (gain - estimated)
+
+                state = next_state
+
+                # print("Q = \n  {}".format(self.Q))
+                # input()
+            
+            else:
+                self.log(reward)
+
+            # report するかどうかの確認
+            if e % report_interval == 0:
+                render = True
+            else : 
+                render = False
+
+def main():
+    """
+    """
+    env = gym.make('MountainCar-v0')
+    video_path = "./Q_video"
+    env = wrappers.Monitor(env, video_path, video_callable=(lambda ep: ep % 100 == 0))
+
+    agent = QlearningAgent()
+    agent.learn(env)
         
-
-
-
+if __name__ == "__main__":
+    main()
